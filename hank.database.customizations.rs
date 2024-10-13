0dd40cfc30e3bdb7385e25db90270e8622b7b7f8
Marc Addeo <marcaddeo@gmail.com>
2024-10-12 21:32:44 -0400

    Move values method to correct impl

--- hank.database.customizations.rs
+++ hank.database.customizations.rs
@@ -1,10 +1,6 @@
 // Customizations from hank.database.customizations.rs
 #[cfg(feature = "builder")]
 impl PreparedStatement {
-    pub fn values(&mut self, value: impl IntoIterator<Item = impl Into<String>>) -> &mut Self {
-        self.values = value.into_iter().map(Into::into).collect();
-        self
-    }
     pub fn new(sql: impl Into<String>) -> PreparedStatementBuilder {
         PreparedStatementBuilder {
             sql: Some(sql.into()),
@@ -14,6 +10,10 @@ impl PreparedStatement {
 }
 #[cfg(feature = "builder")]
 impl PreparedStatementBuilder {
+    pub fn values(&mut self, value: impl IntoIterator<Item = impl Into<String>>) -> &mut Self {
+        self.values = Some(value.into_iter().map(Into::into).collect());
+        self
+    }
     pub fn build(&self) -> PreparedStatement {
         self.fallible_build()
             .expect("All required fields were initialized")
